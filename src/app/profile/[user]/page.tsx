63c55c78ff8f643864a0a24f4e58fa817df180b5
Michał Grocholski <michal3659@gmail.com>
2025-05-28 18:06:24 +0200

    fix(profile): add missing "use client" directive in user dashboard

--- src/app/profile/[user]/page.tsx
+++ src/app/profile/[user]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { FaUsers, FaClock, FaProjectDiagram, FaCalendarCheck, FaChartBar, FaDollarSign, FaChartPie, FaUser } from "react-icons/fa";
