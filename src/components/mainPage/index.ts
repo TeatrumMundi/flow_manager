cbc76571c97fd6308e6891d1368382e3d31eb679
Michał Grocholski <michal3659@gmail.com>
2025-09-09 20:16:33 +0200

    Delete src/components/mainPage directory

--- src/components/mainPage/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Header } from './Header';
-export { default as LoginForm } from './LoginForm';
-export { default as MessageDisplay } from './MessageDisplay';
-export { default as RegisterForm } from './RegisterForm';
-export { default as ResetPasswordForm } from './ResetPasswordForm';
