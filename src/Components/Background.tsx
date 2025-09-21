99fa8cdd096fd8d9405c0631f0f45ecfdc5d69f7
Michał Grocholski <michal3659@gmail.com>
2025-09-21 15:39:09 +0200

    Refactor Background and Input components: wrap Image in a div for proper positioning and add autocomplete attribute to Input based on type Fix icon path in layout metadata

--- src/Components/Background.tsx
+++ src/Components/Background.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 
 export default function Background() {
   return (
-    <Image
-      src="/loginBG.png"
-      alt="Background"
-      fill
-      priority
-      className="object-cover z-[-1] blur-xs"
-      quality={25}
-    />
+    <div className="fixed inset-0 -z-10 w-full h-full">
+      <Image
+        src="/loginBG.png"
+        alt="Background"
+        fill
+        priority
+        className="object-cover blur-xs pointer-events-none"
+        quality={25}
+      />
+    </div>
   );
 }
